39a682129ed9f19b9989e6c060907c76a683629c
xzy773599929 <773599929@qq.com>
2020-03-12 23:38:55 +0800

    06_完整block

--- block.go
+++ block.go
@@ -1,18 +1,34 @@
 package main
 
-import "crypto/sha256"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"time"
+)
 
 //1 .	 定义结构
 type Block struct {
+	Version uint64 //版本号
 	PrevHash []byte  //前区块哈希
+	MerkleRoot []byte //梅克尔根（就是一个哈希值）
+	TimeStamp uint64 //时间戳
+	Difficult uint64 //难度值
+	Nonce uint64 //随机数，挖矿时要找的数
 	Hash []byte  //当前区块哈希
 	Data []byte  //数据
+
 }
 
 //2 .	 创建区块
 func NewBlock(data string,PrevBlockHash []byte)*Block  {
 	block := Block{
-		PrevHash: PrevBlockHash,
+		Version:    00,
+		PrevHash:   PrevBlockHash,
+		MerkleRoot: []byte{},
+		TimeStamp:  uint64(time.Now().Unix()),
+		Difficult:  100,
+		Nonce:      100,
 		Hash:     []byte{},
 		Data:     []byte(data),
 	}
@@ -24,6 +40,12 @@ func NewBlock(data string,PrevBlockHash []byte)*Block  {
 func (block *Block)SetHash()  {
 	//拼装数据
 	blcokInfo := append(block.PrevHash,block.Data...)
+	blcokInfo = append(blcokInfo,block.MerkleRoot...)
+	blcokInfo = append(blcokInfo,Uint64ToByte(block.Version)...)
+	blcokInfo = append(blcokInfo,Uint64ToByte(block.TimeStamp)...)
+	blcokInfo = append(blcokInfo,Uint64ToByte(block.Difficult)...)
+	blcokInfo = append(blcokInfo,Uint64ToByte(block.Nonce)...)
+
 	//sha256
 	hash := sha256.Sum256(blcokInfo)
 	block.Hash = hash[:]
@@ -32,4 +54,15 @@ func (block *Block)SetHash()  {
 //创世区块
 func GenesisBlock() *Block {
 	return NewBlock("创世区块",[]byte{})
+}
+
+//辅助函数，将uint6464转换成[]byte
+func Uint64ToByte(num uint64)[]byte  {
+	var buffer bytes.Buffer
+
+	err := binary.Write(&buffer,binary.BigEndian,num)
+	if err != nil {
+		panic(err)
+	}
+	return buffer.Bytes()
 }
