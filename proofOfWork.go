ddc10ebf6748317d0df56f3d785dbd862faad16e
xzy773599929 <773599929@qq.com>
2020-03-13 09:41:03 +0800

    01_ 引入工作量证明

---
+++ proofOfWork.go
@@ -0,0 +1,23 @@
+package main
+
+import "math/big"
+
+//定义一个工作量证明结构ProofOfWork
+type ProofOfWork struct {
+	block *Block
+	target *big.Int
+}
+
+//创建pow函数
+func NewProofOfWork(block *Block)*ProofOfWork  {
+	pow := ProofOfWork{
+		block:  block,
+	}
+	//自定义难度，写成固定值,现在是string类型
+	targetString := "0000100000000000000000000000000000000000000000000000000000000000"
+	//引入辅助变量，目的是将上面的string转换成big.int类型
+	bigIntTmp := big.Int{}
+	bigIntTmp.SetString(targetString,16)
+	pow.target = &bigIntTmp
+	return &pow
+}
