61a5454f3a5e679270885b1fd0514e820452263b
xzy773599929 <773599929@qq.com>
2020-03-13 13:08:33 +0800

    挖矿功能实现

--- proofOfWork.go
+++ proofOfWork.go
@@ -1,6 +1,11 @@
 package main
 
-import "math/big"
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+)
 
 //定义一个工作量证明结构ProofOfWork
 type ProofOfWork struct {
@@ -24,6 +29,35 @@ func NewProofOfWork(block *Block)*ProofOfWork  {
 
 //不断运行计算哈希函数
 func (pow *ProofOfWork)Run()([]byte,uint64)  {
-	//TODO
-	return []byte("sssssss"),10
+	//定义一个随机数nonce
+	var nonce uint64
+	var hash [32]byte
+	for {
+		//1.拼装数据（区块的数据，还有不断变化的随机数）
+		block := pow.block
+		temp := [][]byte{
+			block.PrevHash,
+			block.MerkleRoot,
+			Uint64ToByte(block.Version),
+			Uint64ToByte(block.TimeStamp),
+			Uint64ToByte(block.Difficult),
+			Uint64ToByte(nonce),
+			block.Data,
+		}
+		blockInfo := bytes.Join(temp,[]byte{})
+		//2.进行哈希运算
+		hash = sha256.Sum256(blockInfo)
+		//3.与pow中的target进行比较
+		//将得到的hash转换成big.int进行比较
+		tmpInt := big.Int{}
+		tmpInt.SetBytes(hash[:])
+		if tmpInt.Cmp(pow.target) < 1 {
+			fmt.Printf("挖矿成功！hash:%x nonce:%d\n",hash,nonce)
+			return hash[:],nonce
+			//找到nonce值，退出循环
+		}else {
+			//没找到，nonce加一，继续循环
+			nonce += 1
+		}
+	}
 }
