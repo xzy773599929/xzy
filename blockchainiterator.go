bf21127d64fc26c40e78b0cb7db62cf75894d9da
xzy773599929 <773599929@qq.com>
2020-03-16 00:16:17 +0800

    实现迭代器读取区块链

--- blockchainiterator.go
+++ blockchainiterator.go
@@ -1,6 +1,9 @@
 package main
 
-import "Block/bolt"
+import (
+	"Block/bolt"
+	"log"
+)
 
 //定义一个读取区块链的迭代器
 type BlockChainIterator struct {
@@ -8,7 +11,7 @@ type BlockChainIterator struct {
 	//哈希指针，用于不断索引
 	currentHashPointer []byte
 }
-
+//创建迭代器函数
 func (bc *BlockChain)NewIterator() *BlockChainIterator  {
 	
 	return &BlockChainIterator{
@@ -16,4 +19,25 @@ func (bc *BlockChain)NewIterator() *BlockChainIterator  {
 		//最初指向区块链中最后一个区块，随着调用Next方法，哈希指针不断前移
 		currentHashPointer: bc.tail,
 	}
+}
+//实现Next函数
+func (it *BlockChainIterator)Next() *Block  {
+	//获取数据库
+	db := it.db
+	block := Block{}
+	//读数据库view
+	db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(blockBucket))
+		if bucket == nil {
+			log.Panic("读bucket为空")
+		}
+		//根据哈希指针获取数据
+		data := bucket.Get(it.currentHashPointer)
+		//数据反序列化获得block结构体
+		block = Deserialize(data)
+		//哈希指针前移
+		it.currentHashPointer = block.PrevHash
+		return nil
+	})
+	return &block
 }
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
+import "fmt"
+
 func main()  {
 	bc := NewBlockChain()
 	bc.AddBlock("许泽洋获得BTC100个")
 	bc.AddBlock("许泽洋获得BTC167个")
 	bc.AddBlock("许泽洋获得BTC24个")
 
-	/*for i , block := range bc.Blocks {
-		fmt.Printf("=====区块高度:%d=====\n",i)
+	//创建迭代器
+	it := bc.NewIterator()
+	//开始循环读取数据库数据
+	for {
+		block := it.Next()
+		fmt.Printf("============================\n\n")
 		fmt.Printf("前一区块哈希:%x\n",block.PrevHash)
 		fmt.Printf("当前区块哈希:%x\n",block.Hash)
 		fmt.Printf("区块内交易数据:%s\n",block.Data)
-	}*/
+		if len(block.PrevHash) == 0 {
+			fmt.Println("迭代器迭代完毕")
+			break
+		}
+	}
 }
