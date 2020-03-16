a13153d74cad8feb3e131480534e924d7612e4ee
xzy773599929 <773599929@qq.com>
2020-03-16 23:36:21 +0800

    完善打印

--- cmdline.go
+++ cmdline.go
@@ -15,7 +15,12 @@ func (cli *CLI)PrintBlockChain()  {
 	for {
 		block := it.Next()
 		fmt.Printf("============================\n\n")
+		fmt.Printf("版本号:%d\n",block.Version)
 		fmt.Printf("前一区块哈希:%x\n",block.PrevHash)
+		fmt.Printf("梅克尔根:%x\n",block.MerkleRoot)
+		fmt.Printf("时间戳:%d\n",block.TimeStamp)
+		fmt.Printf("难度值:%d\n",block.Difficult)
+		fmt.Printf("随机数:%d\n",block.Nonce)
 		fmt.Printf("当前区块哈希:%x\n",block.Hash)
 		fmt.Printf("区块内交易数据:%s\n",block.Data)
 		if len(block.PrevHash) == 0 {
