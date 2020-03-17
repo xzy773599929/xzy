0463f7e3a06163dd41f4bb67c31a72d8c9c40bc0
xzy773599929 <773599929@qq.com>
2020-03-18 00:26:54 +0800

    06_完善反向打印方法

--- cmdline.go
+++ cmdline.go
@@ -15,7 +15,7 @@ func (cli *CLI)PrintBlockChain()  {
 	//开始循环读取数据库数据bl
 	for {
 		block := it.Next()
-		fmt.Printf("============================\n\n")
+		fmt.Printf("\n============================\n")
 		fmt.Printf("版本号:%d\n",block.Version)
 		fmt.Printf("前一区块哈希:%x\n",block.PrevHash)
 		fmt.Printf("梅克尔根:%x\n",block.MerkleRoot)
@@ -33,7 +33,41 @@ func (cli *CLI)PrintBlockChain()  {
 
 //反向打印区块链
 func (cli *CLI)PrintBlockChainReverse()  {
-	//TODO
+	bc := cli.bc
+	//创建迭代器
+	it := bc.NewIterator()
+	//开始循环读取数据库数据
+	i := 1
+	for {
+		block := it.Next()
+		if len(block.PrevHash) == 0 {
+			break
+		}
+		i += 1
+	}
+
+	blocks := make([]*Block,i)
+	it1 := bc.NewIterator()
+	l := 0
+	for {
+		blocks[l] = it1.Next()
+		if len(blocks[l].PrevHash) == 0 {
+			break
+		}
+		l += 1
+	}
+	for m := len(blocks)-1 ; i >= 0 ; i-- {
+		fmt.Printf("\n============================区块高度:%d\n",m)
+		fmt.Printf("版本号:%d\n",blocks[m].Version)
+		fmt.Printf("前一区块哈希:%x\n",blocks[m].PrevHash)
+		fmt.Printf("梅克尔根:%x\n",blocks[m].MerkleRoot)
+		fmt.Printf("时间戳:%d\n",blocks[m].TimeStamp)
+		fmt.Printf("难度值:%d\n",blocks[m].Difficult)
+		fmt.Printf("随机数:%d\n",blocks[m].Nonce)
+		fmt.Printf("当前区块哈希:%x\n",blocks[m].Hash)
+		fmt.Printf("区块内交易数据:%s\n",blocks[m].Transactions[0].TXInputs[0].Sig)
+	}
+	fmt.Println("区块链遍历完毕")
 }
 
 //指定地址获取余额
