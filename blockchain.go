2bee8ad20110dd5286dbb18d50211573ffa4c694
xzy773599929 <773599929@qq.com>
2020-03-19 20:55:36 +0800

    v4_12_优化公共代码

--- blockchain.go
+++ blockchain.go
@@ -98,74 +98,30 @@ func (bc *BlockChain)FindNeedUXTOs(from string,amount float64)(map[string][]uint
 	//找到的utxo里面包含的余额总数
 	var cacl float64
 
-	//1111111111111111111
-	//map[交易id][]索引值
-	spentOutputs := make(map[string][]int64)
-	//创建迭代器
-	it := bc.NewIterator()
-	for {
-		//1.遍历区块
-		block := it.Next()
-		//2.遍历交易
-		for _,tx := range block.Transactions {
-
-		OUTPUT:
-			//3.遍历output，找到和自己地址相关的utxo（在添加output之前检查是否已经消耗过）
-			for i,output := range tx.TXOutputs {
-				//在这里做一个过滤，过滤消耗过的output，进行对比
-				//如果相同，则不添加
-				//如果当前交易的id已存在于map的key中，则说明这个交易里有消耗过的output
-				if spentOutputs[string(tx.TXID)] != nil  {
-					for _,j := range spentOutputs[string(tx.TXID)] {
-						if int64(i) == j {
-							//相等说明当前output已经消耗了，不再添加
-							continue OUTPUT
-						}
+	txs := bc.FindUTXOTransaction(from)
+	for i,tx := range txs {
+		for _,output := range tx.TXOutputs{
+			if output.PubKeyHash == from {
+				if cacl < amount {
+					//1.把utxo加进来
+					utxos[string(tx.TXID)] = append(utxos[string(tx.TXID)],uint64(i))
+					//2.统计当前utxo总价
+					cacl += output.Value
+					//加完后满足条件
+					//3.比较一下是否满足转账需求
+					//	a.满足的话直接返回utxos，cacl
+					//	b.不满足继续统计
+					if cacl >= amount {
+						//break
+						fmt.Printf("找到了满足的金额:%f\n",cacl)
+						return utxos,cacl
 					}
-				}
-
-				//这个output和我们的目标地址相同，满足条件，添加到UTXO数组中
-				if output.PubKeyHash == from {
-					//UTXO = append(UTXO, output)
-					if cacl < amount {
-						//1.把utxo加进来
-						utxos[string(tx.TXID)] = append(utxos[string(tx.TXID)],uint64(i))
-						//2.统计当前utxo总价
-						cacl += output.Value
-						//加完后满足条件
-						//3.比较一下是否满足转账需求
-						//	a.满足的话直接返回utxos，cacl
-						//	b.不满足继续统计
-						if cacl >= amount {
-							//break
-							fmt.Printf("找到了满足的金额:%f\n",cacl)
-							return utxos,cacl
-						}
-					}else {
-						fmt.Printf("不满足转账金额，当前总额:%f,  目标总额:%f\n",cacl,amount)
-					}
+				}else {
+					fmt.Printf("不满足转账金额，当前总额:%f,  目标总额:%f\n",cacl,amount)
 				}
 			}
-
-			//如果当前交易是挖矿交易的话，那么直接跳过，不做遍历
-			if !tx.IsCoinbase() {
-				//4.遍历input，找到自己花费过的utxo集合（把自己消耗过的标记出来）
-				for _,input := range tx.TXInputs {
-					//判断当前input的签名是否属于自己，如果和自己的地址一致，说明这个消费是自己的
-					if input.Sig == from {
-						indexArray := spentOutputs[string(input.TXid)]
-						indexArray = append(indexArray,input.Index)
-						spentOutputs[string(input.TXid)] = indexArray
-					}
-				}
-			}
-		}
-		//跳出遍历区块的循环
-		if len(block.PrevHash) == 0 {
-			break
 		}
 	}
-	//222222222222222
 	return utxos,cacl
 }
 
