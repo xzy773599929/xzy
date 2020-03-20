9b04f4e25e85493f4b6e36f024b75c262754ba42
xzy773599929 <773599929@qq.com>
2020-03-20 23:21:45 +0800

    v5_02_实现loadFile“

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 del block.exe
 del *.db
 del *.lock
-del wallet.dat
 go build -o block.exe ./
--- wallets.go
+++ wallets.go
@@ -6,6 +6,7 @@ import (
 	"encoding/gob"
 	"io/ioutil"
 	"log"
+	"os"
 )
 
 //定义一个Wallets结构，它保存所有wallet以及它的地址
@@ -19,7 +20,8 @@ func NewWallets() *Wallets  {
 	var ws Wallets
 
 	ws.WalletsMap = make(map[string]*Wallet)
-
+	//加载钱包文件
+	ws.loadFile()
 	return &ws
 }
 
@@ -52,3 +54,28 @@ func (ws *Wallets)saveToFile()  {
 }
 
 //读取文件方法，把所有的wallet读出来
+func (ws *Wallets)loadFile()  {
+	//读取内容
+	_,err := os.Stat("wallet.dat")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	content,err := ioutil.ReadFile("wallet.dat")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	//解码
+	var wslocal Wallets
+	//注册
+	gob.Register(elliptic.P256())
+
+	decoder := gob.NewDecoder(bytes.NewReader(content))
+	err = decoder.Decode(&wslocal)
+	if err != nil {
+		log.Panic(err)
+	}
+	//对于struct来说，里面有map的，要指定赋值，不要再最外层赋值 ws=&wslocal（错误）
+	ws.WalletsMap = wslocal.WalletsMap
+}
